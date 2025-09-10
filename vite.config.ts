6aeeb77b268cd64c8836615f9a00f968b5074992
exatombe <jeremy27.clara22@gmail.com>
2025-09-11 00:40:24 +0200

    feat: mettre à jour la configuration Vite pour charger les variables d'environnement

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api/admin': {
-        target: process.env.VITE_API_ADMIN_URL!,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-      '/api/public': {
-        target: process.env.VITE_API_PUBLIC_URL!,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+
+  console.log('Loaded env:', env)
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api/admin': {
+          target: env.VITE_API_ADMIN_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/admin/, ''),
+          secure: false // Ignore la vérification du certificat
+        },
+        '/api/public': {
+          target: env.VITE_API_PUBLIC_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api\/public/, ''),
+          secure: false // Ignore la vérification du certificat
+        },
       },
     },
-  },
+  }
 })
