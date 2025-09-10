c076d3ecf286e41139c5d811b8240e41d3d6f541
exatombe <jeremy27.clara22@gmail.com>
2025-09-11 00:30:13 +0200

    feat: ajouter la configuration du serveur avec proxy pour les API

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api/admin': {
+        target: process.env.VITE_API_ADMIN_URL!,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+      '/api/public': {
+        target: process.env.VITE_API_PUBLIC_URL!,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
